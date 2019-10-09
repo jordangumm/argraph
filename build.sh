d94d1c6fb2af54b696ffaaf27187f7234082a9e6
Jordan Gumm <gumm1jn@cmich.edu>
2019-10-08 20:32:06 -0400

    card data build setup

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 wget https://card.mcmaster.ca/download/5/ontology-v3.0.5.tar.gz
-mkdir data
-tar -zxvf ontology-v3.0.5.tar.gz -C data/
+mkdir -p card/ontology
+tar -zxvf ontology-v3.0.5.tar.gz -C card/ontology
 rm ontology-v3.0.5.tar.gz
+
+wget https://card.mcmaster.ca/download/0/broadstreet-v3.0.5.tar.gz
+mkdir -p card/data
+tar -zxvf broadstreet-v3.0.5.tar.gz -C card/data
+rm broadstreet-v3.0.5.tar.gz
